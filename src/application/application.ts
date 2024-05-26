4df77e93420668862a2b6d99739f4a87f05111e1
Anatoly Dolgov <anatoly.dolgov@uchi.ru>
2024-05-26 19:02:23 +0300

    chore: add Express

--- src/application/application.ts
+++ src/application/application.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'inversify';
+import express from 'express';
 import type {
   Config,
   DBClient,
@@ -9,19 +10,29 @@ import { Component } from '../models/component.enum.js';
 
 @injectable()
 class Application {
+  private readonly server = express();
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<ConfigSchema>,
     @inject(Component.DBClient) private readonly dbClient: DBClient,
   ) {}
 
+  private async initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
-    this.logger.info('App initialized');
-    this.logger.info(`Current port: ${this.config.get('PORT')}`);
+    this.logger.info('Application is initializing…');
 
     this.logger.info('Initialize database…');
     await this.dbClient.connect();
-    this.logger.info('Database initialized');
+    this.logger.info('Database initialized!');
+
+    this.logger.info('Initialize server…');
+    await this.initServer();
+    this.logger.info(`Server is running on port: ${this.config.get('PORT')}!`);
   }
 
   public async close() {
